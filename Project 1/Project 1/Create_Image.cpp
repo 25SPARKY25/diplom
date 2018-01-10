d16201efa7fd46049ec74ef50297b59d3f2a0926
25SPARKY25 <34844380+25SPARKY25@users.noreply.github.com>
2018-01-10 16:48:18 +0300

    Add files via upload

--- Project 1/Project 1/Create_Image.cpp
+++ Project 1/Project 1/Create_Image.cpp
@@ -23,8 +23,8 @@
 
  void Create_Image::void_Cr_image(String^ path, int x)
  {
-	 //enum figures { rectangle, triangle, circle, elipse, NUM_FIGURES};//создаём перечисление геометрических фигур для случайного выбора
-	 enum figures { rectangle, circle, elipse, NUM_FIGURES };//создаём перечисление геометрических фигур для случайного выбора
+	 enum figures { rectangle, triangle, circle, elipse, NUM_FIGURES};//создаём перечисление геометрических фигур для случайного выбора
+	 //enum figures { rectangle, circle, elipse, NUM_FIGURES };//создаём перечисление геометрических фигур для случайного выбора
 	 figures i;
 	 figures fig =static_cast<figures>(rand() % NUM_FIGURES);//генерируем номер для выбора фигуры из перечисления figures
 	 Bitmap ^image = gcnew Bitmap(500, 500);
@@ -35,6 +35,13 @@
 															 //g -> DrawRectangle(BlackPen, 40, 40, 25, 25);//рисуем квадрат
 	 int w = rand() % 250;//размер стороны квадрата
 	 int z = rand() % 250;//размер стороны ограничивающего квадрата
+
+	 Point point1 = Point(rand() % 500, rand() % 500);
+	 Point point2 = Point(rand() % 500, rand() % 500);
+	 Point point3 = Point(rand() % 500, rand() % 500);
+	 cli::array<Point, 1>^ TrianglePoints = { point1, point2, point3 };
+
+
 	 switch (fig){
 		 case 0:  //квадрат
 			 //int w = rand() % 50;//размер стороны квадрата
@@ -43,25 +50,25 @@
 			 image->Save(path + "\\Rectangle" + x+ ".bmp");
 		break;
 	 
-		//case 1:  //треугольник
-		//	// Создаём массив координат для треугольника
-		//	Point point1 = Point(rand() % 500, rand() % 500);
-		//	Point point2 = Point(rand() % 500, rand() % 500);
-		//	Point point3 = Point(rand() % 500, rand() % 500);
-		//	array<Point>^ TrianglePoints = { point1, point2, point3 };
-		//	//Point TrianglePoints[3] = { Point(rand() % 500, rand() % 500),Point(rand() % 500, rand() % 500), Point(rand() % 500, rand() % 500) };
-		//	 g->FillRectangle(Whitebrush, 0, 0, 500, 500);//заливаем цветом фон
-		//	 g->FillPolygon(Blackbrush, TrianglePoints);//заливаем цветом треугольник
-		//break;
+		case 1:  //треугольник
+			// Создаём массив координат для треугольника
+			
+			//TrianglePoints = gcnew array<Point, 1>(3);
 
-		case 1:	//окружность
+			//Point TrianglePoints[3] = { Point(rand() % 500, rand() % 500),Point(rand() % 500, rand() % 500), Point(rand() % 500, rand() % 500) };
+			 g->FillRectangle(Whitebrush, 0, 0, 500, 500);//заливаем цветом фон
+			 g->FillPolygon(Blackbrush, TrianglePoints);//заливаем цветом треугольник
+			 image->Save(path + "\\Triamgle" + x + ".bmp");
+		break;
+
+		case 2:	//окружность
 			//int z = rand() % 250;//размер стороны ограничивающего квадрата
 			g->FillRectangle(Whitebrush, 0, 0, 500, 500);//заливаем цветом фон
 			g->FillEllipse(Blackbrush, rand() % 250, rand() % 250, z, z);//заливаем цветом окружность
 			image->Save(path + "\\Circle" + x + ".bmp");
 			break;
 
-		case 2:	//эллипс
+		case 3:	//эллипс
 			//int w = rand() % 50;//размер стороны ограничивающего квадрата
 			g->FillRectangle(Whitebrush, 0, 0, 500, 500);//заливаем цветом фон
 			g->FillEllipse(Blackbrush, rand() % 250, rand() % 250, rand() % 250, rand() % 250);//заливаем цветом треугольник
